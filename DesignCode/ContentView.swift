562377e3d1f9fb2255397229c9dc40bca6756c39
Robert van Steen <hello@rovansteen.nl>
2020-06-26 23:43:56 +0200

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Robert on 26/06/2020.
+//  Copyright © 2020 Robert van Steen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
