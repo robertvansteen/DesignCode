b45923fbb9365019ad5ac60585b50392f76991b7
Robert van Steen <hello@rovansteen.nl>
2020-06-27 00:23:25 +0200

    feat: Section 2 - Layout and Stacks

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -10,7 +10,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            VStack {
+                Spacer()
+            }
+            .frame(width: 300, height: 220)
+            .background(Color.blue)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            .offset(x: 0, y: -20)
+            
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("UI Design")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                        Text("Certificate")
+                            .foregroundColor(Color("accent"))
+                    }
+                    Spacer()
+                    Image("Logo1")
+                }
+                .padding(.horizontal, 20)
+                .padding(.top, 20)
+                Spacer()
+                Image("Card1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 300, height: 110, alignment: .top)
+            }
+            .frame(width: 340.0, height: 220.0)
+            .background(Color.black)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+        }
     }
 }
 
