7d34a9ac4f7e693e1e592e28e5ace252626b89f6
Robert van Steen <hello@rovansteen.nl>
2020-06-28 22:35:39 +0200

    feat: Section 7 - Dismiss and Drag Anchors

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     @State var show = false
     @State var viewState = CGSize.zero
     @State var showCard = false
+    @State var bottomState = CGSize.zero
+    @State var showFull = false
     
     var body: some View {
         ZStack {
@@ -79,8 +81,33 @@ struct ContentView: View {
             
             BottomCardView()
                 .offset(x: 0, y: showCard ? 360 : 1000)
+                .offset(y: bottomState.height)
                 .blur(radius: show ? 20 : 0)
                 .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+                .gesture(
+                    DragGesture().onChanged { value in
+                        self.bottomState = value.translation
+                        if self.showFull {
+                            self.bottomState.height += -300
+                        }
+                        if self.bottomState.height < -300 {
+                            self.bottomState.height = -300
+                        }
+                    }
+                    .onEnded { value in
+                        if self.bottomState.height > 50 {
+                            self.showCard = false
+                        }
+                        
+                        if (self.bottomState.height < -100 && !self.showFull) || (self.bottomState.height < -250 && self.showFull) {
+                            self.bottomState.height = -300
+                            self.showFull = true
+                        } else {
+                            self.bottomState = CGSize.zero
+                            self.showFull = false
+                        }
+                    }
+                )
         }
     }
 }
