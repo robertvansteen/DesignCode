ca3458120a6c56068b20c74fcaf646492e0fa429
Robert van Steen <hello@rovansteen.nl>
2020-06-28 12:06:34 +0200

    feat: Section 5 - Gestures and Events

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var show = false
+    @State var viewState = CGSize.zero
     
     var body: some View {
         ZStack {
@@ -22,6 +23,7 @@ struct ContentView: View {
                 .cornerRadius(20)
                 .shadow(radius: 20)
                 .offset(x: 0, y: show ? -400 : -40)
+                .offset(x: viewState.width, y: viewState.height)
                 .scaleEffect(0.9)
                 .rotationEffect(.degrees(show ? 0 : 10))
                 .rotation3DEffect(.degrees(10), axis: (x: 10, y: 0, z: 0))
@@ -33,6 +35,7 @@ struct ContentView: View {
                 .cornerRadius(20)
                 .shadow(radius: 20)
                 .offset(x: 0, y: show ? -200 : -20)
+                .offset(x: viewState.width, y: viewState.height)
                 .scaleEffect(0.95)
                 .rotationEffect(.degrees(show ? 0 : 5))
                 .rotation3DEffect(.degrees(5), axis: (x: 10, y: 0, z: 0))
@@ -40,10 +43,21 @@ struct ContentView: View {
                 .animation(.easeInOut(duration: 0.3))
             
             CardView()
+                .offset(x: viewState.width, y: viewState.height)
                 .blendMode(.hardLight)
+                .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
                 .onTapGesture {
                     self.show.toggle()
+            }.gesture(
+                DragGesture().onChanged { value in
+                    self.viewState = value.translation
+                    self.show = true
                 }
+                .onEnded { value in
+                    self.viewState = .zero
+                    self.show = false
+                }
+            )
             
             BottomCardView()
                 .blur(radius: show ? 20 : 0)
