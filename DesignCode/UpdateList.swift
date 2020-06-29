fde9cae9288e5b816b4efa7b3637bbccdb8717c8
Robert van Steen <hello@rovansteen.nl>
2020-06-29 21:11:22 +0200

    feat: Section 17 - Navigation View and List

---
+++ DesignCode/UpdateList.swift
@@ -0,0 +1,66 @@
+//
+//  UpdateList.swift
+//  DesignCode
+//
+//  Created by Robert on 29/06/2020.
+//  Copyright © 2020 Robert van Steen. All rights reserved.
+//
+
+import SwiftUI
+
+struct UpdateList: View {
+    var body: some View {
+        NavigationView {
+            List(updateData) { update in
+                NavigationLink(destination: Text(update.text)) {
+                    HStack {
+                        Image(update.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 80, height: 80)
+                            .background(Color.black)
+                            .cornerRadius(20)
+                            .padding(.trailing, 4)
+                        
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text(update.title).font(.system(size: 20, weight: .bold))
+                            
+                            Text(update.text)
+                                .font(.subheadline)
+                                .lineLimit(2)
+                                .foregroundColor(.secondary)
+                            
+                            Text(update.date)
+                                .font(.caption)
+                                .fontWeight(.bold)
+                                .foregroundColor(.secondary)
+                        }
+                    }.padding(.vertical, 8)
+                }
+            }
+            .navigationBarTitle(Text("Updates"))
+        }
+    }
+}
+
+struct UpdateList_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateList()
+    }
+}
+
+struct Update: Identifiable {
+    var id = UUID()
+    var image: String
+    var title: String
+    var text: String
+    var date: String
+}
+
+let updateData = [
+    Update(image: "Card1", title: "SwiftUI Advanced", text: "Take your SwiftUI app to the App Store with advanced techniques like API data, packages and CMS.", date: "JAN 1"),
+    Update(image: "Card2", title: "Webflow", text: "Design and animate a high converting landing page with advanced interactions, payments and CMS", date: "OCT 17"),
+    Update(image: "Card3", title: "ProtoPie", text: "Quickly prototype advanced animations and interactions for mobile and Web.", date: "AUG 27"),
+    Update(image: "Card4", title: "SwiftUI", text: "Learn how to code custom UIs, animations, gestures and components in Xcode 11", date: "JUNE 26"),
+    Update(image: "Card5", title: "Framer Playground", text: "Create powerful animations and interactions with the Framer X code editor", date: "JUN 11")
+]
