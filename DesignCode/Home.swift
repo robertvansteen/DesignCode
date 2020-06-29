87d55123684ab1a37d8d3de4f8033f414cd30893
Robert van Steen <hello@rovansteen.nl>
2020-06-29 09:35:15 +0200

    feat: Section 11 - Drag Progress and Tap Background

--- DesignCode/Home.swift
+++ DesignCode/Home.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct Home: View {
     @State var showProfile = false
+    @State var viewState = CGSize.zero
     
     var body: some View {
         ZStack {
@@ -33,7 +34,7 @@ struct Home: View {
                             .clipShape(Circle())
                     }
                 }
-                .padding(.horizontal, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                .padding(.horizontal)
                 .padding(.top, 30)
                 
                 Spacer()
@@ -43,14 +44,29 @@ struct Home: View {
             .clipShape(RoundedRectangle(cornerRadius: 30, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
             .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
             .offset(y: showProfile ? -450 : 0)
-            .rotation3DEffect(Angle(degrees: showProfile ? -10 : 0), axis: (x: 10, y: 0, z: 0))
+            .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10, y: 0, z: 0))
             .scaleEffect(showProfile ? 0.9 : 1)
             .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
             .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             
             MenuView()
-                .offset(y: showProfile ? 0 : 600)
+                .background(Color.black.opacity(0.001))
+                .offset(y: showProfile ? 0 : 1000)
+                .offset(y: viewState.height)
                 .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+                .onTapGesture {
+                    self.showProfile.toggle()
+                }
+                .gesture(
+                    DragGesture().onChanged { value in
+                        self.viewState = value.translation
+                    }.onEnded { value in
+                        if self.viewState.height > 50 {
+                            self.showProfile = false
+                        }
+                        self.viewState = CGSize.zero
+                    }
+                )
         }
     }
 }
